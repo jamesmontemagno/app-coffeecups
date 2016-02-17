a854a7cc8e5fbc6d430e226e4459801a45589bd4
James Montemagno <james.montemagno@gmail.com>
2016-02-17 16:26:27 -0700

    Add UWP authentication

---
+++ UWP/CoffeeCups.UWP/Helpers/Authentication.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using Microsoft.WindowsAzure.MobileServices;
+using System.Threading.Tasks;
+using CoffeeCups.Helpers;
+using Xamarin.Forms;
+using CoffeeCups.UWP;
+
+[assembly:Dependency(typeof(Authentication))]
+namespace CoffeeCups.UWP
+{
+    public class Authentication : IAuthentication
+    {
+        public async Task<MobileServiceUser> LoginAsync(MobileServiceClient client, MobileServiceAuthenticationProvider provider)
+        {
+            try
+            {
+                var user = await client.LoginAsync(provider);
+
+                Settings.AuthToken = user?.MobileServiceAuthenticationToken ?? string.Empty;
+                Settings.UserId = user?.UserId ?? string.Empty;
+
+                return user;
+            }
+            catch(Exception e)
+            {
+                e.Data["method"] = "LoginAsync";
+                Xamarin.Insights.Report(e);
+            }
+
+            return null;
+        }
+
+        public void ClearCookies()
+        {
+            
+        }
+    }
+}
+
